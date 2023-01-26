a848bc9460461eb7854d341c4c1690a507020314
gastonsartori <sartorigastonemilio@gmail.com>
2023-01-26 12:06:58 -0300

    modulo de segmentacion IF/ID

---
+++ tp_final.srcs/sources_1/new/IF_ID.v
@@ -0,0 +1,32 @@
+`timescale 1ns / 1ps
+
+module IF_ID#(
+       parameter   NB_PC = 32,         //Cantidad de bits del PC
+       parameter   NB_INSTR = 32       // Ancho de bits de las instrucciones
+)(
+       input                       i_if_id_clock,                
+       input                       i_if_id_reset,
+       input                       i_if_id_write_enable,         //Señal desde la Stall Unit para actuualizar o no los registros
+       input                       i_if_id_flush,                //Señal para descartar los registros (en caso de un branch)
+       input  [NB_PC-1:0]          i_if_id_pc,
+       input  [NP_INSTR-1:0]       i_if_id_instruction,
+
+       output reg [NB_PC-1:0]          o_if_id_pc,
+       output reg [NP_INSTR-1:0]       o_if_id_instruction                               
+);
+
+always@(posedge i_if_id_clock) 
+begin
+       if(i_if_id_reset || i_if_id_flush)
+       begin
+              o_if_id_pc <= 0;
+              o_if_id_instruction <=0;
+       end
+       else if(i_if_id_write_enable) // Si la Stall Unit habilita la escritura
+       begin 
+              o_if_id_pc <= i_if_id_pc;
+              o_if_id_instruction <= i_if_id_instruction;
+       end
+end
+    
+endmodule
