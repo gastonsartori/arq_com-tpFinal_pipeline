8c540538441f4c82ff92a50387f8ff4dc81b69d2
gastonsartori <sartorigastonemilio@gmail.com>
2023-01-30 12:47:11 -0300

    etapa ID

---
+++ tp_final.srcs/sources_1/new/Banco_registros.v
@@ -0,0 +1,127 @@
+`timescale 1ns / 1ps
+
+module Banco_registros#(
+
+    parameter   NB_DATA     =   32,     //Cantidad de bits de los registros     
+    parameter   REG_DEPTH   =   32,      //Cantidad de registros
+    parameter   NB_ADDR     = $clog2(REG_DEPTH)     // Log base 2 de la cantidad de registros para asi direccionarlos
+)
+(
+
+    input                           i_regbank_clock, 
+    input                           i_regbank_reset, 
+    input                           i_regbank_RegWrite,   //Señal de control que habilita la escritura sobre el banco de registros
+    input                           i_regbank_enable,       //Señal de control que habilita la lectura del banco de registros
+    input [NB_ADDR - 1 : 0]     i_regbank_rA,                   //Entrada de direccionamiento del primer registro a leer (ID)
+    input [NB_ADDR - 1 : 0]     i_regbank_rB,                   //Entrada de direccionamiento del segundo registro a leer (ID)
+    input [NB_ADDR - 1 : 0]     i_regbank_rW,               //Entrada de direccionamiento del registro a escribir (WB)
+    input [NB_DATA - 1 : 0]     i_regbank_dataW,               //Entrada del dato a escribir (WB) 
+            
+    output [NB_DATA - 1 : 0]    o_regbank_dataA, o_regbank_dataB
+);
+
+//Banco de registros - REG_DEPTH registros de NB_DATA bits
+reg     [NB_DATA - 1 : 0]   registers   [REG_DEPTH - 1 : 0];
+
+reg    [NB_DATA - 1 : 0]    regbank_dataA, regbank_dataA_next;
+reg    [NB_DATA - 1 : 0]    regbank_dataB, regbank_dataB_next;
+
+assign o_regbank_dataA = regbank_dataA;
+assign o_regbank_dataB = regbank_dataB;
+
+// Lectura(actualizacion de la salida) en el negedge para leer los datos ya actualizados
+/*
+always@(negedge i_regbank_clock)
+begin
+    if(i_regbank_enable)
+    begin
+        regbank_dataA <= regbank_dataA_next;
+        regbank_dataB <= regbank_dataB_next;
+    end
+end
+
+always@(*)
+begin
+    if(i_regbank_reset)
+    begin
+        reset_all();
+    end
+    else
+    begin
+        regbank_dataA_next = regbank_dataA;
+        regbank_dataB_next = regbank_dataB;
+        //if(i_enable)
+        //begin
+            if(i_regbank_RegWrite) //Escritura
+            begin
+                registers[i_regbank_rW] = i_regbank_dataW;
+            end
+            if(i_regbank_rW == i_regbank_rA) //Si la direccon de ra o rb es igual a la direccion de escritura, se coloca el contenido a la salida para evitar dependencias de WB
+            begin
+                regbank_dataA_next = i_regbank_dataW;
+                regbank_dataB_next = registers[i_regbank_rB];
+            end
+            else if(i_regbank_rW == i_regbank_rB)
+            begin
+                regbank_dataA_next = registers[i_regbank_rA];
+                regbank_dataB_next = i_regbank_dataW;
+            end
+            else
+            begin
+                regbank_dataA_next = registers[i_regbank_rA];
+                regbank_dataB_next = registers[i_regbank_rB];
+            end
+    end
+    
+end
+*/
+
+//PROBAR ESTA IMPLEMENTACION
+
+always @(posedge i_regbank_clock)
+begin
+    if(i_regbank_reset)
+    begin
+        reset_all();
+    end
+    else
+    begin
+        if (i_regbank_RegWrite) 
+        begin
+            registers[i_regbank_rW] <= i_regbank_dataW;
+        end
+    end
+end
+
+always @(negedge i_regbank_clock)
+begin
+    if(i_regbank_enable)
+    begin
+        regbank_dataA <= registers[i_regbank_rA];
+        regbank_dataB <= registers[i_regbank_rB];
+    end
+end
+
+task reset_all;
+    begin:resetReg
+        integer reg_index;
+        for (reg_index = 0; reg_index < REG_DEPTH; reg_index = reg_index + 1)
+          registers[reg_index] = {NB_DATA{1'b0}};
+        regbank_dataA =  {NB_DATA{1'b0}};
+        regbank_dataB  =  {NB_DATA{1'b0}};
+    end
+endtask
+
+/*    
+task reset_all;
+    begin:resetReg
+        integer reg_index;
+        for (reg_index = 0; reg_index < REG_DEPTH; reg_index = reg_index + 1)
+          registers[reg_index] = {NB_DATA{1'b0}};
+        regbank_dataA_next  =  {NB_DATA{1'b0}};
+        regbank_dataB_next  =  {NB_DATA{1'b0}};
+    end
+endtask
+*/
+    
+endmodule
---
+++ tp_final.srcs/sources_1/new/ID.v
@@ -0,0 +1,126 @@
+`timescale 1ns / 1ps
+
+module ID#(
+    parameter   NB_PC = 32,         //Cantidad de bits del PC
+    parameter   NB_INSTR = 32,       // Ancho de bits de las instrucciones
+    
+
+    parameter   NB_RD = 5,          //Cantidad de bits del campo rd en las instrucciones
+    parameter   NB_RT = 5,          //Cantidad de bits del campo rt en las instrucciones
+    parameter   NB_RS = 5,          //Cantidad de bits del campo rs en las instrucciones
+    parameter   NB_FUNCTION = 6,    //Cantidad de bits del campo funct en las instrucciones
+    parameter   NB_OFFSET = 16,     //Cantidad de bits del campo inmediato en las instrucciones
+    parameter   NB_DIR = 26,        //Cantidad de bits del campo direccion (instr_index) en las instrucciones
+    parameter   NB_OP = 6,          //Cantidad de bits del campo op en las instrucciones
+
+    parameter   NB_REG = 32,        // Cantidad de bits de los registros
+
+    parameter   NB_ALUOP = 4,   //Cantidad de bits para determinar el tipo de operacion 
+    
+    //Bits de inicio de los campos dentro de la instruccion
+    parameter   B_START_RS = 21,
+    parameter   B_START_RT = 16,
+    parameter   B_START_RD = 11,
+    parameter   B_START_OFFSET = 0,
+    parameter   B_START_FUNCTION = 0,
+    parameter   B_START_OP = 26
+        
+)(
+    input [NB_PC-1:0]       i_id_pc,            //PC desde IF/ID
+    input [NB_INSTR-1:0]    i_id_instruction,   //instruccion a decodificar
+    input                   i_id_clock,
+    input                   i_id_reset,
+    input [NB_RD-1:0]       i_id_write_reg,             //Registro en donde se hara la escritura (WB)
+    input [NB_REG-1:0]      i_id_write_data,            //Dato a escribir (WB)
+    input                   i_id_RegWrite,              //Señal de control desde MEM/WB, habilita o no la escritura
+    input                   i_id_branch,                //Señal de control desde EX/MEM, si es una instruccion de branch, va a la unidad de control
+    input                   i_id_zero,                  //Señal desde EX/MEM, si la salida de la ALU fue cero, va a la unidad de control
+    input                   i_id_control_enable,        //Señal desde StallUnit, para no modificar las señales de control, va a la unidad de control
+    input                   i_id_write_enable,          //Señal desde StallUnit, para no actualizar la salida del banco de registros
+
+    output [NB_PC - 1 :0]       o_id_pc,                    //PC a ID/EX   
+    output [NB_REG - 1 :0]      o_id_dataA, o_id_dataB,     //Datos leidos desde el banco de registros
+    output [NB_REG - 1 :0]      o_id_offset_ext,            //Offset con el signo extendido
+    output [NB_RT - 1 :0]       o_id_rt,                    //campo rt
+    output [NB_RD - 1 :0]       o_id_rd,                    //campo rd
+    output [NB_RS - 1 :0]       o_id_rs,                    //campo rs
+
+    //Señales de control para las diferentes etapas
+    //IF
+    output [1:0]            o_id_PcSrc,    //especifica que entrada del mux sera el nuevo PC
+    //EX
+    output [1:0]            o_id_RegDst,   //especifica cual es el indentificador del registro destino (0->rt,1->rd)
+    output [1:0]            o_id_ALUSrc,   //ALUSrc[0] especifica cual es el primer operando de la ALU (0->rs, 1->rt)
+                                                    //ALUSrc[1]especifica cual es el segundo operando de la ALU (0->rt, 1->campo inmediato (offset))
+    output [NB_ALUOP-1:0]     o_id_ALUOp,    
+    //MEM
+    output                  o_id_MemRead,  //habilita la lectura de memoria
+    output                  o_id_MemWrite, //habilita la escritura de memoria
+    output                  o_id_Branch,   //especifica si la instruccion es un branch o no
+    //WB
+    output                  o_id_RegWrite, //habilita o no la escritura en el banco de registros
+    output [1:0]            o_id_MemtoReg, //especifica cual es la fuente al escribir en registros (0->ALU,1->memoria)
+
+    //Señales de Flush para los regsitros de segmentacion en caso de branch
+    output             o_id_IF_ID_Flush,
+    output             o_id_EX_MEM_Flush  
+);
+
+wire [NB_RT - 1 :0] rt;
+wire [NB_RD - 1 :0] rd;
+wire [NB_RS - 1 :0] rs;
+
+assign rt = i_id_instruction[B_START_RT+NB_RT-1 : B_START_RT];
+assign rs = i_id_instruction[B_START_RS+NB_RS-1 : B_START_RS];
+assign rd = i_id_instruction[B_START_RD+NB_RD-1 : B_START_RD];
+
+assign o_id_pc = i_id_pc;
+assign o_id_rt = rt;
+assign o_id_rs = rs;
+assign o_id_rd = rd;
+
+
+Banco_registros Registers
+(
+    .i_regbank_clock(i_id_clock), 
+    .i_regbank_reset(i_id_reset), 
+    .i_regbank_RegWrite(i_id_RegWrite), 
+    .i_regbank_enable(i_id_write_enable),
+    .i_regbank_rA(rs), //campo RS de la instruccion [25:21]
+    .i_regbank_rB(rt), //campo RT de la instruccion [20:16]
+    .i_regbank_rW(i_id_write_reg),
+    .i_regbank_dataW(i_id_write_data),
+         
+    .o_regbank_dataA(o_id_dataA), 
+    .o_regbank_dataB(o_id_dataB) 
+);
+
+
+Ext_signo Ext_signo
+(
+    .i_signext(i_id_instruction[B_START_OFFSET + NB_OFFSET - 1 : B_START_OFFSET]),
+    .o_signext(o_id_offset_ext)   
+);
+
+Unidad_control Control
+(
+    .i_controlunit_enable(i_id_control_enable),
+    .i_controlunit_zero(i_id_zero), 
+    .i_controlunit_branch(i_id_branch),
+    .i_controlunit_op(i_id_instruction[B_START_OP+NB_OP-1 : B_START_OP]),
+    .i_controlop_funct(i_id_instruction[B_START_FUNCTION+NB_FUNCTION-1 : B_START_FUNCTION]),
+
+    .o_controlunit_PcSrc(o_id_PcSrc),
+    .o_controlunit_RegDst(o_id_RegDst),
+    .o_controlunit_ALUSrc(o_id_ALUSrc),
+    .o_controlunit_ALUOp(o_id_ALUOp),
+    .o_controlunit_MemRead(o_id_MemRead), 
+    .o_controlunit_MemWrite(o_id_MemWrite),
+    .o_controlunit_Branch(o_id_Branch),
+    .o_controlunit_RegWrite(o_id_RegWrite),
+    .o_controlunit_MemtoReg(o_id_MemtoReg),
+    .o_controlunit_IF_ID_Flush(o_id_IF_ID_Flush),
+    .o_controlunit_EX_MEM_Flush(o_id_EX_MEM_Flush) 
+);
+
+endmodule
